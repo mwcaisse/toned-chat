c1d97eb83c3e6c49721ce5e774172e1ae70cf7f3
Mitchell Caisse <mitchell@mwcaisse.com>
2025-02-14 09:11:05 -0500

    I have a button

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,12 +1,24 @@
 
+import {Button, Center, Container, TextInput} from "@mantine/core";
+
 function App() {
 
     return (
         <>
-            <div>
-                <h2>Welcome to Toned Chat</h2>
-                <p>Hello</p>
-            </div>
+            <Container>
+                <Center>
+                    <p>Welcome to Toned Chat!</p>
+                </Center>
+                <Center>
+                    What shall we call you?
+                </Center>
+                <Center>
+                    <TextInput
+                        placeholder="Name"
+                    />
+                    <Button variant="filled">Get Started</Button>
+                </Center>
+            </Container>
         </>
     )
 }
