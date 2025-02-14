c21c32834618fcd6d1de3194ee42a676a3caf66e
Mitchell Caisse <mitchell@mwcaisse.com>
2025-02-14 08:56:10 -0500

    Added matine. Configured eslint for quoting/indent

--- frontend/src/main/main.tsx
+++ frontend/src/main/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "@app/index.css"
-import App from "@app/App.tsx";
+import "@mantine/core/styles.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "@app/App.tsx";
+import {createTheme, MantineProvider} from "@mantine/core";
+
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
+    </StrictMode>,
 )
